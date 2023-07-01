d089a24f8ef0962db0c28b989d7321489919e8d0
Carlos Freitas <carlosantonioenem23@gmail.com>
2023-07-01 01:49:45 -0300

    random color

--- js/change.js
+++ js/change.js
@@ -1,16 +1,26 @@
 let button = document.querySelector("#botão");
 
-
 button.addEventListener("click", () => {
-    let colors = ["#928288", "#61bdc2", "#e11a00", "#6ea743", "#56913b", "#3d8b45", "#e6765e", "#d1644b", "#b1533d", "#c9eaf8", "#a9d8e9", "#82c9e0", "#7a07d9", "#4d00d9", "#8d8d8d", "#999999", "#33cc40", "#ffff99", "#fcea94", "#fff1b1", "#ffe464", "#ffcc66", "#fbd37b", "#805af2", "#c00c00", "#96e100", "#a275b8", "#763b60", "#ac6589", "#805af2", "#113333", "#133133", "#000133", "#ff4f00", "#6565bf", "#db7093", "#b5b35c", "#9ab973", "#6082b6", "#e5aa70", "#9966cc", "#cc8855"]
+    /*   let colors = ["#928288", "#61bdc2", "#e11a00", "#6ea743", "#56913b", "#3d8b45", "#e6765e", "#d1644b", "#b1533d", "#c9eaf8", "#a9d8e9", "#82c9e0", "#7a07d9", "#4d00d9", "#8d8d8d", "#999999", "#33cc40", "#ffff99", "#fcea94", "#fff1b1", "#ffe464", "#ffcc66", "#fbd37b", "#805af2", "#c00c00", "#96e100", "#a275b8", "#763b60", "#ac6589", "#805af2", "#113333", "#133133", "#000133", "#ff4f00", "#6565bf", "#db7093", "#b5b35c", "#9ab973", "#6082b6", "#e5aa70", "#9966cc", "#cc8855"] */
+    var sb = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var hex = ''
 
-    let randomColor = Math.floor(Math.random() * colors.length);
+    for (i = 0; i < 6; i++) {
+       
+
+        let randomColor = Math.floor(Math.random() * sb.length);
+
+        
+
+        hex += sb[randomColor]
+
+        
+    }
 
     let bd = document.getElementsByTagName("body")[0];
+        bd.style.transition = '.5s ease'
+        bd.style.background = "#" + hex;
+        /*  console.log(colors[randomColor]); */
 
-    bd.style.transition = '.5s ease'
-    bd.style.background = colors[randomColor];
-    console.log(randomColor);
-
-    /*  console.log(colors.length) */
+        /*  console.log(colors.length) */
 })
